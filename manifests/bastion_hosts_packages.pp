1cd9525632b19677da0cc57e4431a1641a5ad85a
Daniel Fernández <daniel.fernandez@epfl.ch>
2023-10-12 15:30:10 +0200

    [HELP-17165] Add postgresql client to bastion host packages list

--- manifests/bastion_hosts_packages.pp
+++ manifests/bastion_hosts_packages.pp
@@ -1,5 +1,5 @@
 class aws_poc::bastion_hosts_packages () {
-  $packages = [ 'strace', 'nmap-ncat', 'nmap', 'wget', 'vim', 'tmux', 'tcpdump', 'net-tools', 'curl', 'jq', 'nfs-utils', 'zsh', 'bash' ]
+  $packages = [ 'strace', 'nmap-ncat', 'nmap', 'wget', 'vim', 'tmux', 'tcpdump', 'net-tools', 'curl', 'jq', 'nfs-utils', 'zsh', 'bash', 'postgresql' ]
   package { $packages:
     ensure => 'installed'
   }
