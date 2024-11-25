a2144d45ef0e36cae38f1918abd494967c54b928
Daniel Fernandez <daniel.fernandez@epfl.ch>
2024-11-25 13:00:13 +0100

    NOTICKET Replace shapes-registry.org EFS CNAME by EFS DNS name

--- manifests/filesystem_compute_efs.pp
+++ manifests/filesystem_compute_efs.pp
@@ -16,7 +16,7 @@ class aws_poc::filesystem_compute_efs () {
     name    => '/sbo/home',
     atboot  => false,
     fstype  => 'nfs',
-    device  => 'compute-efs.shapes-registry.org:/',
+    device  => 'fs-0c2a2f3ad1b1beeca.efs.us-east-1.amazonaws.com:/',
     options => 'nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport',
   }
 }
