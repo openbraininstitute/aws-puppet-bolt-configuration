3d9ed3018b35bd7698b612ae9195f7deec3843c0
Daniel Fernandez <daniel.fernandez@epfl.ch>
2024-12-03 11:39:26 +0100

    [NOTICKET] Replace shapes-registry.org by openbluebrain.com

--- run-ci-container.sh
+++ run-ci-container.sh
@@ -9,7 +9,7 @@ echo "❗ssh-agent with AWS coreservices key from pass needed❗"
 
 # Install nc, so that it's possible to use the ssh bastion host to forward ssh connections
 # to other hosts, such as the sbo-poc-compute01 HPC test VM.
-ssh ec2-user@ssh.shapes-registry.org sudo yum install -y nmap-ncat dnf-automatic
+ssh ec2-user@ssh.openbluebrain.com sudo yum install -y nmap-ncat dnf-automatic
 
 # Puppet bolt always first attempts to install the puppet agent on a node to gather facts
 # before it attempts to run any manifests. Normally it does this automatically, but this
@@ -18,11 +18,11 @@ ssh ec2-user@ssh.shapes-registry.org sudo yum install -y nmap-ncat dnf-automatic
 #    features: puppet-agent block in the inventory.yaml.
 # Currently disabled as at the moment we're no longer applying puppet manifest changes on
 # the head node: it's done by a python script made by Omar.
-#ssh ec2-user@sbo-poc-pcluster.shapes-registry.org sudo rpm -Uvh https://yum.puppet.com/puppet7-release-el-7.noarch.rpm
-#ssh ec2-user@sbo-poc-pcluster.shapes-registry.org sudo yum install puppet-agent -y
+#ssh ec2-user@sbo-poc-pcluster.openbluebrain.com sudo rpm -Uvh https://yum.puppet.com/puppet7-release-el-7.noarch.rpm
+#ssh ec2-user@sbo-poc-pcluster.openbluebrain.com sudo yum install puppet-agent -y
 
-scp run-bastion-host.sh ec2-user@ssh.shapes-registry.org:/tmp/
-ssh ec2-user@ssh.shapes-registry.org bash /tmp/run-bastion-host.sh
+scp run-bastion-host.sh ec2-user@ssh.openbluebrain.com:/tmp/
+ssh ec2-user@ssh.openbluebrain.com bash /tmp/run-bastion-host.sh
 
 bolt module install
 bolt plan run aws_poc --native-ssh -v
